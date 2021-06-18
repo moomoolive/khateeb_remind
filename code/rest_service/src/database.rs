90862479527e1c02f93e3b817c4544c35e324cfd
Mostafa Elbannan <mostafaelbannan1@gmail.com>
2021-06-18 16:48:18 -0600

    got first route to work with actix service

---
+++ code/rest_service/src/database.rs
@@ -0,0 +1,55 @@
+use mongodb::{ 
+    Client, 
+    options::ClientOptions, 
+    Collection,
+};
+use bson::{ doc, Document };
+use tokio::stream::StreamExt;
+
+const TARGET_DATABASE: &'static str = "khateebRemind";
+const AUTH_TOKEN_COLLECTION: &'static str = "resttokens";
+
+#[derive(Debug, Clone)]
+pub struct ConnectionOptions<'a> {
+    pub uri: &'a str,
+    pub client_name: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct DatabaseInterfaces {
+    client: Client
+}
+
+impl DatabaseInterfaces {
+    pub async fn new(options: &ConnectionOptions<'_>) -> Self {
+        let mut client_options = ClientOptions::parse(options.uri)
+            .await
+            .unwrap();
+        client_options.app_name = Some(options.client_name.clone());
+        let client = Client::with_options(client_options).unwrap();
+        DatabaseInterfaces { client }
+    }
+
+    fn target_collection(&self, collection: &str) -> Collection {
+        self.client
+            .database(TARGET_DATABASE)
+            .collection(collection)
+    }
+
+    fn auth_token_collection(&self) -> Collection {
+        self.target_collection(AUTH_TOKEN_COLLECTION)
+    }
+
+    pub async fn find_auth_token(&self) -> Vec<Document> {
+        let collection = self.auth_token_collection();
+        let mut cursor = collection.find(doc!{}, None).await.unwrap();
+        let mut auth_tokens = Vec::new();
+        while let Some(doc) = cursor.next().await {
+            match doc {
+                Ok(d) => auth_tokens.push(d),
+                _ => panic!("no doc")
+            }
+        }
+        auth_tokens
+    }
+}
--- code/rest_service/src/main.rs
+++ code/rest_service/src/main.rs
@@ -1,36 +1,59 @@
-use actix_web::{ get, App, HttpResponse, HttpServer, Result };
+mod database;
+
+use database::{ ConnectionOptions, DatabaseInterfaces };
+
+use actix_web::{ get, App, HttpResponse, HttpServer, Result, web };
 use serde::{ Deserialize, Serialize };
 use actix_cors::Cors;
 
 use std::env::{ set_var };
 
-/// # Main entry point of server
+#[derive(Debug)]
+struct AppState {
+    db: web::Data<DatabaseInterfaces>
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     set_var("RUST_LOG", "actix_web=debug");
-
-    HttpServer::new(|| {
+    let database_config = ConnectionOptions {
+        uri: "mongodb://localhost:27017/",
+        client_name: String::from("rest_service")
+    };
+    let db = DatabaseInterfaces::new(&database_config).await;
+    
+    HttpServer::new(move || {
         // allow all CORS policy
         let cors = Cors::permissive();
-         
+
         App::new()
             .wrap(cors)
+            .data(AppState {
+                db: web::Data::new(db.clone())
+            })
             .service(hello) 
     })
-        .bind("0.0.0.0:80")?
-        .run()
-        .await
+    .bind("0.0.0.0:80")?
+    .run()
+    .await
 }
 
 #[derive(Serialize, Deserialize)]
-struct MyObj {
-    name: String,
+struct MyObj<T> {
+    data: Vec<T>,
+    msg: i8
 }
 
 #[get("/")]
-async fn hello() -> Result<HttpResponse> {
-    let res = MyObj { name: String::from("Hello world") };
-    Ok(HttpResponse::Ok().json(MyObj {
-        name: res.name,
-    }))
+async fn hello(data: web::Data<AppState>) -> Result<HttpResponse> {
+    let tokens = data
+        .as_ref()
+        .db
+        .find_auth_token()
+        .await;
+    let res = MyObj {
+        data: tokens,
+        msg: 0
+    };
+    Ok(HttpResponse::Ok().json(res))
 }
